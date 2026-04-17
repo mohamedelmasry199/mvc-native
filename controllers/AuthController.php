5f0b92ded9780d3ce964fd83f4fe551fe05058aa
unknown <mohamedelmasry1992001@gmail.com>
2026-04-17 11:50:41 +0200

    model prepere valaditions and rules

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,16 +2,43 @@
 namespace app\controllers;
 use app\core\Application;
 use app\core\Controller;
+use app\core\Request;
+use app\models\RegisterModel;
 
 class AuthController extends Controller {
     public function login()
     {
+
         $this->setLayout('auth');
         return $this->render('login');
     }
-    public function register()
+    public function register(Request $request)
     {
+        $errors = [];
+        $registerModel = new RegisterModel();
+        if($request->isPost()) {
+            $registerModel->loadData($request->getBody());
+
+
+            if($registerModel->validate() && $registerModel->register()) {
+                return 'success';
+            }
+            echo "<pre>";
+            var_dump($registerModel->errors);
+            echo "</pre>";
+            exit;
+              return $this->render('register',[
+            'model' => $registerModel
+        ]);
+            // $body = $request->getBody();
+            // echo "<pre>";
+            // var_dump($body);
+            // echo "</pre>";
+            // exit;
+        }
         $this->setLayout('auth');
-        return $this->render('register');
+        return $this->render('register',[
+            'model' => $registerModel
+        ]);
     }
 }
--- views/register.php
+++ views/register.php
@@ -1,9 +1,10 @@
 <h2 class="mb-4">Register</h2>
-
-<form method="POST" action="index.php?page=register">
-    <input type="text" name="name" class="form-control mb-3" placeholder="Name" required>
+<form method="POST" action="register">
+    <input type="text" name="firstname" class="form-control mb-3" placeholder="First Name" required>
+    <input type="text" name="lastname" class="form-control mb-3" placeholder="Last Name" required>
     <input type="email" name="email" class="form-control mb-3" placeholder="Email" required>
     <input type="password" name="password" class="form-control mb-3" placeholder="Password" required>
+    <input type="password" name="passwordConfirm" class="form-control mb-3" placeholder="Confirm Password" required>
 
     <button class="btn btn-success">Register</button>
 </form>
